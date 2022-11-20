4049e690e4efef7704f07bfa237638a39c27f1be
Matthew McGowan <matthew@mcmcgowan.com>
2022-11-20 09:24:58 -1000

    added login page

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import LogIn from "./components/LogIn.jsx";
 
 const App = () =>{
     return (
-        <h1>
-            Welcome to React App thats build using Webpack and Babel separately
-        </h1>
+        <div>
+            <h1>
+                Welcome to React App thats build using Webpack and Babel separately
+            </h1>
+            <LogIn />        
+        </div>
     )
 }
 
--- src/client/index.js
+++ src/client/index.js
@@ -5,7 +5,6 @@ import "./styles/main.scss";
 import store from './store.js'
 import { Provider } from "react-redux";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
