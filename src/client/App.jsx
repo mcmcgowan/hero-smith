ce86de0876f1751a7debae2d9b8e76cd85f24484
Matthew McGowan <matthew@mcmcgowan.com>
2022-11-20 15:22:13 -1000

    added navbar

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import LogIn from "./components/LogIn.jsx";
+import NavBar from "./components/NavBar.jsx";
 
 const App = () =>{
     return (
         <div>
+            <NavBar />
             <h1>
                 Welcome to React App thats build using Webpack and Babel separately
             </h1>
-            <LogIn />        
+            <LogIn />     
         </div>
     )
 }
